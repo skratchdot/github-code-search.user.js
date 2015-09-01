9c38f1728e87044e0dd1f7335e03515dac6f454e
skratchdot <git@skratchdot.com>
2015-09-01 14:54:35 -0400

    please use github-enhancement-suite instead

--- github-code-search.user.js
+++ github-code-search.user.js
@@ -4,27 +4,12 @@
 // @description    A user script that adds a search box to repository pages which allows you to search the code in that repository.
 // @include        https://github.com/*
 // @match          https://github.com/*
-// @require        https://gist.github.com/skratchdot/5604120/raw/_init.js
-// @require        https://gist.github.com/skratchdot/5604120/raw/code-search.js
 // @run-at         document-end
 // @icon           http://skratchdot.com/favicon.ico
 // @downloadURL    https://github.com/skratchdot/github-code-search.user.js/raw/master/github-code-search.user.js
 // @updateURL      https://github.com/skratchdot/github-code-search.user.js/raw/master/github-code-search.user.js
-// @version        2.4
+// @version        3.0
 // ==/UserScript==
-/*global SKRATCHDOT, document */
 
-// This code is only going to run for browsers that don't support
-// the @require annotation when executing userscripts.
-if ('undefined' === typeof SKRATCHDOT) {
-	var addScript = function (src) {
-		'use strict';
-		var script = document.createElement('script');
-		script.src = src;
-		document.body.appendChild(script);
-		document.body.removeChild(script);
-	};
-
-	// Required by: repo-filter-info
-	addScript('https://gist.github.com/skratchdot/5604120/raw/code-search.js');
-}
+console.log('This script has been updated and is part of the Github Enhancement Suite' +
+		' found at: https://github.com/skratchdot/github-enhancement-suite');
